75e3ef0f1da4d03deb5dd46c7eaab30fb634411b
erichmond_33 <eerichmond33@gmail.com>
2024-08-19 16:19:14 -0500

    x

--- website/models.py
+++ website/models.py
@@ -1,5 +1,7 @@
+from typing import Any
 from django.db import models
 from django.core.files.storage import default_storage
+from django.db import transaction
 
 # Create your models here.
 class Watch(models.Model):
@@ -20,3 +22,33 @@ class Watch(models.Model):
             if old_instance.image and old_instance.image != self.image:
                 default_storage.delete(old_instance.image.path)
         super().save(*args, **kwargs)
+
+    @transaction.atomic
+    def delete(self, *args, **kwargs):
+        # Delete secondary images
+        for secondary_image in self.secondary_images.all():
+            secondary_image.delete()
+        
+        # Delete the main image
+        if self.image:
+            default_storage.delete(self.image.path)
+        
+        super().delete(*args, **kwargs)
+
+class WatchSecondaryImage(models.Model):
+    watch = models.ForeignKey(Watch, related_name='secondary_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            # Get the old instance
+            old_instance = WatchSecondaryImage.objects.get(pk=self.pk)
+            # Delete the old image
+            if old_instance.image and old_instance.image != self.image:
+                default_storage.delete(old_instance.image.path)
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            default_storage.delete(self.image.path)
+        super().delete(*args, **kwargs)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Watch
+from .models import *
 from django.shortcuts import redirect
 
 # Create your views here.
@@ -12,10 +12,17 @@ def add_view(request):
     if request.method == 'POST':
         image = request.FILES['image']
         name = request.POST['name']
-        price = request.POST['price']
+        price = request.POST['price'].replace(',', '')
         year = request.POST['year']
 
         watch = Watch.objects.create(name=name, price=price, year=year, image=image)
+        
+        try: 
+            images = request.FILES.getlist('images')
+            for image in images:
+                WatchSecondaryImage.objects.create(watch=watch, image=image)
+        except:
+            pass
 
         return redirect('edit', watch.id)
     elif request.method == 'GET':
@@ -31,10 +38,17 @@ def edit_view(request, watch_id):
                 watch.image = image
         except:
             pass
+
+        try: 
+            images = request.FILES.getlist('images')
+            for image in images:
+                WatchSecondaryImage.objects.create(watch=watch, image=image)
+        except:
+            pass
         name = request.POST['name']
         if name:
             watch.name = name
-        price = request.POST['price']
+        price = request.POST['price'].replace(',', '')
         if price:
             watch.price = price
         year = request.POST['year']
@@ -56,3 +70,21 @@ def checkout_view(request):
 def inventory_view(request):
     watches = Watch.objects.all()
     return render(request, 'website/inventory.html', {'watches': watches})
+
+def watch_view(request, watch_id):
+    watch = Watch.objects.get(id=watch_id)
+    images = [watch.image] + [image.image for image in watch.secondary_images.all()]
+    return render(request, 'website/watch.html', {'watch': watch, 'images': images})
+
+def delete_image(request, image_id):
+    if request.method == 'POST':
+        image = WatchSecondaryImage.objects.get(id=image_id)
+        watch_id = image.watch.id
+        image.delete()
+        return redirect('edit', watch_id)
+
+def delete_watch(request, watch_id):
+    if request.method == 'POST':
+        watch = Watch.objects.get(id=watch_id)
+        watch.delete()
+        return redirect('inventory')
