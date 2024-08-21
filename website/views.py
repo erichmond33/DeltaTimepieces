d5d30af15bcf87170f331632033c297de5326e1d
erichmond_33 <eerichmond33@gmail.com>
2024-08-21 16:41:59 -0500

    cart

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import redirect
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -65,7 +66,27 @@ def edit_view(request, watch_id):
 
 def checkout_view(request):
     if request.method == 'GET':
-        return render(request, 'website/checkout.html')
+        cart = request.session.get('cart', [])
+        watches_in_cart = Watch.objects.filter(id__in=cart)
+        total = sum([watch.price for watch in watches_in_cart])
+
+        return render(request, 'website/checkout.html', {'cart': watches_in_cart, 'total': total})
+    
+def add_to_cart(request, watch_id):
+        cart = request.session.get('cart', [])
+        if watch_id not in cart:
+            cart.append(watch_id)
+            request.session['cart'] = cart
+
+        return redirect('checkout')
+
+def remove_from_cart(request, watch_id):
+    cart = request.session.get('cart', [])
+    if watch_id in cart:
+        cart.remove(watch_id)
+        request.session['cart'] = cart
+
+    return redirect('checkout')
     
 def inventory_view(request):
     watches = Watch.objects.all()
