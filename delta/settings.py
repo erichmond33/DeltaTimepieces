f3f1181afb8eea7effd19fb97f94d5f871701538
erichmond_33 <eerichmond33@gmail.com>
2024-08-28 11:36:07 -0500

    heroku

--- delta/settings.py
+++ delta/settings.py
@@ -139,6 +139,13 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'static'
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
