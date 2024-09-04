d51f444d20bb83cd495e5126ec69e802cbb029d6
erichmond_33 <eerichmond33@gmail.com>
2024-09-04 18:18:47 -0500

    images

--- website/utils/context_processors.py
+++ website/utils/context_processors.py
@@ -3,5 +3,5 @@ from ..models import *
 def cart_processor(request):
     cart = request.session.get('cart', [])
     watches_in_cart = Watch.objects.filter(id__in=cart)
-    total = str(sum([watch.price for watch in watches_in_cart])) + " + shipping"
+    total = sum([watch.price for watch in watches_in_cart])
     return {'cart': watches_in_cart, 'total': total}
